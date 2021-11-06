04cc97b60a4ac96fb9e810637fff2ed7d25a51d6
syedmurtaza108 <murtaza.hussain@koderlabs.com>
2021-11-06 19:39:04 +0500

    Add project files.

---
+++ Angry Professor/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angry_Professor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                string[] nk = Console.ReadLine().Split(' ');
+
+                int n = Convert.ToInt32(nk[0]);
+
+                int k = Convert.ToInt32(nk[1]);
+
+                int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp))
+                ;
+                string result = angryProfessor(k, a);
+
+                Console.WriteLine(result);
+            }
+        }
+
+        static string angryProfessor(int k, int[] a)
+        {
+            int goodStudents = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] <= 0)
+                    goodStudents++;
+            }
+            if (goodStudents >= k)
+                return "NO";
+            else
+                return "YES";
+        }
+    }
+}
